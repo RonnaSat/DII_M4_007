d15367c77da46da55643ae9e63a0397105eab9a1
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-05-18 14:36:59 +0700

    i&b

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function alert1() {
-    alert("let me see ...");
-}
-let ok = document.getElementById("ok")
-let cancel = document.getElementById("cancel")
-let showDate = document.getElementById("showDate")
-ok.addEventListener("click", function (e) {
-    alert("let me see ...")
-})
-cancel.addEventListener("click", function (e) {
-    alert("not ok ...")
-})
-showDate.addEventListener("click", function (e) {
-    document.getElementById("emtySpace").innerText = Date();
-})
-showDate.addEventListener("dblclick", function (e) {
-    document.getElementById("emtySpace").innerText = "";
-})
+// function alert1() {
+//     alert("let me see ...");
+// }
+// let ok = document.getElementById("ok")
+// let cancel = document.getElementById("cancel")
+// let showDate = document.getElementById("showDate")
+// ok.addEventListener("click", function (e) {
+//     alert("let me see ...")
+// })
+// cancel.addEventListener("click", function (e) {
+//     alert("not ok ...")
+// })
+// showDate.addEventListener("click", function (e) {
+//     document.getElementById("emtySpace").innerText = Date();
+// })
+// showDate.addEventListener("dblclick", function (e) {
+//     document.getElementById("emtySpace").innerText = "";
+// })
+var newtext = "<i><b>Ronnakon<b><i>";
+document.getElementById("no").innerHTML = newtext;
