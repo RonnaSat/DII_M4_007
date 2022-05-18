9790c3912868c4e02fba8ffe13084fd67f85534a
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-05-18 12:19:10 +0700

    click

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const newText = "not ok";
-document.getElementById("ok").innerHTML = newText;
+function alert1(){
+    alert("let me see ...");
+}
+let ok = document.getElementById("ok")
+let cancel = document.getElementById("cancel")
+ok.addEventListener("click", function(e){
+    alert("let me see ...")
+})
+cancel.addEventListener("click", function(e){
+    alert("not ok ...")
+})
